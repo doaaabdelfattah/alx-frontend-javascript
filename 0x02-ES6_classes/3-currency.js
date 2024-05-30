cf154a8a4c2e28f166cb723da80fe839202528df
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-05-30 17:57:47 +0300

    “advanced”

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -15,15 +15,15 @@ export default class Currency {
   // ======> Setters
 
   set name(name) {
-    if (typeof name !== "string") {
-      throw new TypeError("Name must be a string");
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
 
   set code(code) {
-    if (typeof code !== "string") {
-      throw new TypeError("Code must be a string");
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
     }
     this._code = code;
   }
