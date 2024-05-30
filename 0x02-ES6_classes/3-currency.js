910d6e5bb3869b9a216e73a6a62ef42a4621c66e
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-05-30 12:29:45 +0300

    “task2”

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,3 +1,41 @@
 export default class Currency {
+  constructor(code, name) {
+    // // Validation
+    // if (typeof name !== 'string' || typeof code !== 'string') {
+    //   throw new TypeError('Name must be a string');
+    // }
 
+    this._code = code;
+    this._name = name;
+  }
+
+  // ======> Getters
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+  // ======> Setters
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  //= =====> Methods
+
+  displayFullCurrency() {
+    return (`${this._name} (${this._code})`);
+  }
 }
