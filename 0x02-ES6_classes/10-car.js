775449b690721f48497fb7c78b813c86921fc639
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-05-30 17:54:25 +0300

    “advanced”

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -4,12 +4,18 @@ export default class Car {
     this._motor = motor;
     this._color = color;
   }
+  /* Car defines a static getter for Symbol.species that returns this. This means by default, Car methods that use Symbol.species will use Car for creating new instances.
+  The cloneCar method uses this.constructor[Symbol.species] to determine the constructor to use for creating a new instance.
+  this.constructor refers to the constructor of the current instance. If this is an instance of a subclass of Car, this.constructor will be the subclass constructor.
+  this.constructor[Symbol.species] accesses the Symbol.species property of the constructor, which by default is Car, but can be overridden in subclasses.
+  
+  */
 
-  // static get [Symbol.species]() {
-  //   return this;
-  // }
+  static get [Symbol.species]() {
+    return this;
+  }
 
   cloneCar() {
-    return new this.constructor();
+    return new this.constructor[Symbol.species];
   }
 }
