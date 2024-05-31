3c41c71d6a013845639c19759074bb238751d329
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-05-31 17:35:49 +0300

    “upload_repo”

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([createUser(), uploadPhoto()]).then((value) => {
+  return Promise.all([createUser(), uploadPhoto()]).then((value) => {
     console.log(value[1].body, value[0].firstName, value[0].lastName);
   }).catch(() => console.log('Signup system offline'));
 }
