66f8511dd41e20c5673d112841418805cf2daf20
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-05-31 17:06:21 +0300

    “upload_repo”

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    const obj = {
+      status: 200,
+      body: 'Success',
+    };
+
+    if (success === true) {
+      resolve(obj);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
